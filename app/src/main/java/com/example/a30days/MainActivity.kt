5cd1b0ea71835355a137edccdc4f287c7766fcd0
j4n9k4 <janukananayakkara@gmail.com>
2025-02-28 01:26:20 +0530

    Added the item card

--- app/src/main/java/com/example/a30days/MainActivity.kt
+++ app/src/main/java/com/example/a30days/MainActivity.kt
@@ -4,14 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.a30days.data.Item
+import com.example.a30days.data.Items
 import com.example.a30days.ui.theme._30DaysTheme
+import androidx.compose.ui.unit.dp
+import androidx.core.app.NotificationCompat.Style
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +35,39 @@ class MainActivity : ComponentActivity() {
 
             }
         }
+    }
+}
+
+@Composable
+fun DayItem(
+    dayItem: Item,
+    modifier: Modifier = Modifier)
+{
+    Card(
+        modifier = modifier.height(450.dp)
+    ) {
+        Column(
+
+        ) {
+            Text(
+                text = stringResource(dayItem.description)
+            )
+            Image(
+                painter = painterResource(dayItem.image),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .weight(1f)
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun DayAppPreview()
+{
+    _30DaysTheme {
+        DayItem(Items.itemList[15])
     }
 }
