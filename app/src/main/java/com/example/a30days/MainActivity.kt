ed5b7455f74440965232b8428c2602f892ffc85a
j4n9k4 <janukananayakkara@gmail.com>
2025-02-28 01:39:50 +0530

    Added the fonts to the theme

--- app/src/main/java/com/example/a30days/MainActivity.kt
+++ app/src/main/java/com/example/a30days/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -50,7 +51,8 @@ fun DayItem(
 
         ) {
             Text(
-                text = stringResource(dayItem.description)
+                text = stringResource(dayItem.description),
+                style = MaterialTheme.typography.displayLarge
             )
             Image(
                 painter = painterResource(dayItem.image),
@@ -68,6 +70,7 @@ fun DayItem(
 fun DayAppPreview()
 {
     _30DaysTheme {
-        DayItem(Items.itemList[15])
+        DayItem(Items.itemList[0],Modifier.padding(top = 50.dp)
+        )
     }
 }
--- app/src/main/java/com/example/a30days/ui/theme/Type.kt
+++ app/src/main/java/com/example/a30days/ui/theme/Type.kt
@@ -2,18 +2,28 @@ package com.example.a30days.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.a30days.R
 
 // Set of Material typography styles to start with
+val Sigmar = FontFamily(
+    Font(R.font.sigmar_regular)
+)
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+    displayLarge = TextStyle(
+        fontFamily = Sigmar,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Sigmar,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
